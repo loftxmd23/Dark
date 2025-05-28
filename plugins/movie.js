c7c8315f191c814ccdd484d9883e8e21f8697214
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-28 21:59:04 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, reply, sender, args }) => {
             from,
             {
                 image: { 
-                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster : 'https://i.ibb.co/4ZSYvPTq/lordali.jpg'
+                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster :'https://files.catbox.moe/fltpob.jpeg'
                 },
                 caption: dec,
                 contextInfo: {
@@ -62,8 +62,8 @@ async (conn, mek, m, { from, reply, sender, args }) => {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363207624903731@newsletter',
-                        newsletterName: '𝐓𝐎𝐇𝐈𝐃 𝐓𝐄𝐂𝐇 🤖',
+                        newsletterJid: '120363398106360290@newsletter',
+                        newsletterName: '𝐷𝛥𝑅𝛫 𝛭𝛥𝑇𝑅𝛪𝛸',
                         serverMessageId: 143
                     }
                 }
