b7da801d44987011edebee4c9159329ae717252b
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-28 20:56:05 +0300

    Update fancy-text.js

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚃𝙾𝙷𝙸𝙳_𝙼𝙳*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *©𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝐥𝐨𝐟𝐭-𝐱𝐦𝐝*`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
