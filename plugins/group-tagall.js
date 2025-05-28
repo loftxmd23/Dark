38d7bdc704771ca5e4a5147bc6b94da4a917feb7
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-28 21:05:33 +0300

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ TOHID ┃ MD ✪──";
+        teks += "└──✪ loft ┃ xmd ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
