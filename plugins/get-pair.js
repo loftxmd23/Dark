4f6a9823df7d93ab4483925a0d689e6cd3a3e5ba
LOFT-XMD-S23 <sheddysheddy236@gmail.com>
2025-05-28 20:59:59 +0300

    Update get-pair.js

--- plugins/get-pair.js
+++ plugins/get-pair.js
@@ -5,9 +5,9 @@ cmd({
     pattern: "pair",
     alias: ["getpair", "clonebot"],
     react: "✅",
-    desc: "Get pairing code for TOHID_MD bot",
+    desc: "Get pairing code for 𝐷𝛥𝑅𝛫 𝛭𝛥𝑇𝑅𝛪𝛸",
     category: "download",
-    use: ".pair 917849917350",
+    use: ".pair 255610150148",
     filename: __filename
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, senderNumber, reply }) => {
     try {
@@ -27,7 +27,7 @@ cmd({
         }
 
         const pairingCode = response.data.code;
-        const doneMessage = "> *TOHID_MD PAIRING COMPLETED*";
+        const doneMessage = "> *𝐷𝛥𝑅𝛫 𝛭𝛥𝑇𝑅𝛪𝛸 PAIRING COMPLETED*";
 
         // Send initial message with formatting
         await reply(`${doneMessage}\n\n*Your pairing code is:* ${pairingCode}`);
